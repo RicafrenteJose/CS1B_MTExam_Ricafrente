c7f66ca41aa0aee64258f02ca5477a642a44b7af
RicafrenteJose <198441580+RicafrenteJose@users.noreply.github.com>
2025-03-20 21:31:00 +0800

    Update userProfile.js

--- userProfile.js
+++ userProfile.js
@@ -1,11 +1,29 @@
-let first_name= prompt("Enter your first name:");
-let last_name = prompt("Enter yiur last name:");
-let Age = prompt("Enter your age:");
-let fav_number = prompt("Enter your favorite number:");
-let fav_color = prompt("Enter you favorite color:");
+// Ask the user to enter their first name and store it in a variable
+let first_name = prompt("Enter your first name:");
 
+// Ask the user to enter their last name and store it in a variable
+let last_name = prompt("Enter your last name:");
+
+// Ask the user to enter their age and store it in a variable
+let Age = prompt("Enter your age:");
+
+// Ask the user to enter their favorite number and store it in a variable
+let fav_number = prompt("Enter your favorite number:");
+
+// Ask the user to enter their favorite color and store it in a variable
+let fav_color = prompt("Enter your favorite color:");
+
+// Print the first name to the console
 console.log(first_name);
+
+// Print the last name to the console
 console.log(last_name);
+
+// Print the age to the console
 console.log(Age);
+
+// Print the favorite number to the console
 console.log(fav_number);
+
+// Print the favorite color to the console
 console.log(fav_color);
